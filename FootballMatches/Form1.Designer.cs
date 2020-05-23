32e654b3df6ad8b25ab304cacd91edf0df7a2aa2
Dinozaur_G <karnah@mail.ru>
2020-05-23 19:13:21 +0700

    ds

--- FootballMatches/Form1.Designer.cs
+++ FootballMatches/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelForMatches = new System.Windows.Forms.Label();
+            this.labelForResults = new System.Windows.Forms.Label();
+            this.lambda = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAction = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelForMatches);
+            this.panel1.Controls.Add(this.labelForResults);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(485, 484);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelForMatches
+            // 
+            this.labelForMatches.Location = new System.Drawing.Point(3, 2);
+            this.labelForMatches.Name = "labelForMatches";
+            this.labelForMatches.Size = new System.Drawing.Size(246, 465);
+            this.labelForMatches.TabIndex = 1;
+            // 
+            // labelForResults
+            // 
+            this.labelForResults.Location = new System.Drawing.Point(250, 2);
+            this.labelForResults.Name = "labelForResults";
+            this.labelForResults.Size = new System.Drawing.Size(232, 272);
+            this.labelForResults.TabIndex = 0;
+            // 
+            // lambda
+            // 
+            this.lambda.Location = new System.Drawing.Point(594, 13);
+            this.lambda.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.lambda.Name = "lambda";
+            this.lambda.Size = new System.Drawing.Size(120, 22);
+            this.lambda.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(504, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Лямбда";
+            // 
+            // buttonAction
+            // 
+            this.buttonAction.Location = new System.Drawing.Point(507, 56);
+            this.buttonAction.Name = "buttonAction";
+            this.buttonAction.Size = new System.Drawing.Size(207, 126);
+            this.buttonAction.TabIndex = 3;
+            this.buttonAction.Text = "Сыграем в футбол?";
+            this.buttonAction.UseVisualStyleBackColor = true;
+            this.buttonAction.Click += new System.EventHandler(this.buttonAction_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(726, 508);
+            this.Controls.Add(this.buttonAction);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lambda);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.lambda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelForMatches;
+        private System.Windows.Forms.Label labelForResults;
+        private System.Windows.Forms.NumericUpDown lambda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAction;
     }
 }
 
--- FootballMatches/Form1.cs
+++ FootballMatches/Form1.cs
@@ -12,9 +12,100 @@ namespace FootballMatches
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        private void buttonAction_Click(object sender, EventArgs e)
+        {
+            labelForMatches.Text = "";
+            labelForResults.Text = "";
+            double lmbd = (double)lambda.Value;
+            int[] teams = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[,] matches = new int[8,8];
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    double[] possibilites = new double[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                    for (int k = 0; k < 15; k++)
+                    {
+                        int m = 0;
+                        double S = 0;
+                        while (m < 10)
+                        {
+                            double a = rnd.NextDouble() % 1;
+                            S += Math.Log(a);
+                            if (S < -lmbd)
+                                break;
+                            else
+                                m++;
+                        }
+                        if (m >= 10)
+                            continue;
+                        else
+                        {
+                            possibilites[m] = (Math.Pow(lmbd, m) * Math.Pow(Math.E, -lmbd) / fact(m));
+                        }
+                    }
+                    for (int k = 0; k < 10; k++)
+                    {
+                        double a = rnd.NextDouble() % 1;
+                        a -= possibilites[k];
+                        if (a <= 0)
+                        {
+                            matches[i,j] = k;
+                            break;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = i + 1; j < 8; j++)
+                {
+                    labelForMatches.Text += "Команда " + (i + 1) + " " + matches[i, j] + " - " + matches[j,i] + " " + "Команда" + (j + 1) + "\n";
+                    if(matches[i, j] > matches[j, i])
+                    {
+                        teams[i] += 2;
+                    }
+                    else if (matches[i, j] < matches[j, i])
+                    {
+                        teams[j] += 2;
+                    }
+                    else
+                    {
+                        teams[i]++;
+                        teams[j]++;
+                    }
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                int max = 0;
+                int teamMax = 0;
+                for (int j = 0; j < 8; j++)
+                {
+                    if (teams[j] > max)
+                    {
+                        max = teams[j];
+                        teamMax = j + 1;
+                    }
+                }
+                labelForResults.Text += "Команда " + teamMax + " - " + (max - 2) + " Очков" + "\n";
+                teams[teamMax - 1] = 0;
+            }
+        }
+        public int fact(int m)
+        {
+            int factorial = 1;
+            for(int g = 1; g <= m; g++)
+            {
+                factorial *= g;
+            }
+            return factorial;
         }
     }
 }
